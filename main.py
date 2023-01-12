ea04246a9afbe8822dce74129b05c64ddaebffbb
MistressMetal <57451414+MistressMetal@users.noreply.github.com>
2023-01-12 14:54:58 -0500

    Update main.py
    Adding server line

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 with open('data/pa_counties.json') as f:
     gj = geojson.load(f)
